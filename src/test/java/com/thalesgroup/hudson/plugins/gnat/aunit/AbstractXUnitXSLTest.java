4206f077dd9f65ebdf0ae9d442054312bf58990c
gbois <gbois@dev.java.net>
2010-08-29 08:58:26 +0000

    Upgrade to xUnit 1.7

--- src/test/java/com/thalesgroup/hudson/plugins/gnat/aunit/AbstractXUnitXSLTest.java
+++ src/test/java/com/thalesgroup/hudson/plugins/gnat/aunit/AbstractXUnitXSLTest.java
@@ -24,8 +24,9 @@
 package com.thalesgroup.hudson.plugins.gnat.aunit;
 
 
+import com.thalesgroup.dtkit.metrics.api.InputMetric;
+import com.thalesgroup.dtkit.metrics.api.InputMetricFactory;
 import com.thalesgroup.dtkit.metrics.api.InputMetricXSL;
-import com.thalesgroup.dtkit.util.converter.ConvertUtil;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Assert;
@@ -44,19 +45,21 @@ public class AbstractXUnitXSLTest {
 
 
     public void convertAndValidate(Class<? extends InputMetricXSL> classType, String inputXMLPath, String expectedResultPath) throws Exception {
-        InputMetricXSL inputMetricXSL = classType.newInstance();
+
+        InputMetric inputMetric = InputMetricFactory.getInstance(classType);
+                                
         File outputXMLFile = File.createTempFile("result", "xml");
         File inputXMLFile = new File(this.getClass().getResource(inputXMLPath).toURI());
 
         //The input file must be valid
-        Assert.assertTrue(inputMetricXSL.validateInputFile(inputXMLFile));
+        Assert.assertTrue(inputMetric.validateInputFile(inputXMLFile));
 
-        ConvertUtil.convert(inputMetricXSL.getClass(), inputMetricXSL.getXslName(), inputXMLFile, outputXMLFile);
+        inputMetric.convert(inputXMLFile, outputXMLFile);
         Diff myDiff = new Diff(XSLUtil.readXmlAsString(new File(this.getClass().getResource(expectedResultPath).toURI())), XSLUtil.readXmlAsString(outputXMLFile));
         Assert.assertTrue("XSL transformation did not work" + myDiff, myDiff.similar());
 
         //The generated output file must be valid
-        Assert.assertTrue(inputMetricXSL.validateOutputFile(outputXMLFile));
+        Assert.assertTrue(inputMetric.validateOutputFile(outputXMLFile));
 
         outputXMLFile.deleteOnExit();
     }
